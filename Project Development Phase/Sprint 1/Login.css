5f74a922332d194c8843c52ae53dcea955283024
SINDHUJA.I <115224162+19tuec227@users.noreply.github.com>
2022-10-29 15:38:40 +0530

    Create Login.css

---
+++ Project Development Phase/Sprint 1/Login.css
@@ -0,0 +1,110 @@
+html,
+body {
+	height: 100%;
+	margin: 0;
+	font-weight: 300;
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+		Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.wrapper {
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.loginContainer {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+	min-width: 25rem;
+	padding: 1rem 3rem;
+	border: 1px solid #44444444;
+	box-shadow: 0px 3px 2px 1px #44444444;
+	border-radius: 8px;
+}
+
+.loginContainer span {
+	text-align: center;
+	font-size: 3rem;
+	font-weight: 500;
+	margin: 1rem 1rem 3rem;
+}
+
+.traditionalLoginContainer form {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+.traditionalLoginContainer :is(input[type="text"], input[type="password"], input[type="email"]) {
+	margin: 0.3rem;
+	padding: 0.3em 0.5em;
+	border: 1px solid #44444444;
+	border-radius: 5px;
+	outline: none;
+	min-width: 200px;
+	font-size: 1.3rem;
+}
+
+.traditionalLoginContainer .loginButton {
+	background-color: #0070f3;
+	font-size: 1.6rem;
+	padding: 0.2em 0.8em;
+	color: white;
+	margin: 0.4rem;
+	border: none;
+	border-radius: 5px;
+    cursor: pointer;
+	margin-top: 2rem;
+}
+
+.traditionalLoginContainer .loginButton:hover {
+    background-color: #0071f3d6;
+}
+
+.loginWithFireContainer {
+	display: grid;
+	display: -ms-grid;
+	place-items: center;
+}
+
+.fire {
+	background-color: #f8f9fa;
+	border: 1px solid #3c404321;
+	border-radius: 4px;
+	color: #3c4043;
+	font-family: arial, sans-serif;
+
+	margin: 11px 4px;
+	padding: 0.4em 0.8em;
+	line-height: 27px;
+
+	min-width: 54px;
+	text-align: center;
+	cursor: pointer;
+	user-select: none;
+	font-size: 1.3rem;
+	font-weight: 500;
+}
+
+.hyperLink {
+	text-decoration: none;
+    text-align: center;
+    font-size: 1.2rem;
+    color: #0070f3;
+    font-weight: 400;
+}
+
+@media screen and (max-width: 480px) {
+    .loginContainer {
+        border: none;
+        box-shadow: none;
+        min-width: fit-content;
+        min-width: -moz-fit-content;
+        min-width: -webkit-fill-available;
+        padding: 1rem;
+    }
+}
