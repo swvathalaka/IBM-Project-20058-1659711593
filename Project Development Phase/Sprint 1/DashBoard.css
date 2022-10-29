48fb541c2ceabfb531e4119e6b9cb876278330c3
SINDHUJA.I <115224162+19tuec227@users.noreply.github.com>
2022-10-29 15:37:59 +0530

    Create DashBoard.css

---
+++ Project Development Phase/Sprint 1/DashBoard.css
@@ -0,0 +1,89 @@
+html, body {
+    margin: 0;
+    padding: 0;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    
+}
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    margin: 0 auto;
+}
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    background-color: #f1f1f1;
+}
+.heading {
+    font-size: 1.3rem;
+}
+.right {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+}
+.actionCenter {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 1rem;
+    padding: 0.5rem 0;
+}
+
+.actionCenter .action {
+    padding: 0.5rem 0.8rem;
+    border: 1px solid #44444444;
+    border-radius: 12px;
+    margin-right: 0.5rem;
+    cursor: pointer;
+}
+
+.actionCenter .action:hover {
+    background-color: #0070f3;
+    color: white;
+}
+
+.childCardContainer {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0.5rem 1rem;
+}
+
+.childCardContainer .childCard {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #44444444;
+    border-radius: 12px;
+    max-width: 300px;
+    font-size: 1.4rem;
+}
+
+.childCardContainer .childCard .childCardHeader {
+    display: flex;
+    gap: 3rem;
+    margin: 0.1rem 0.1rem 0.8rem;
+    padding: 1rem 0.8rem 0;
+}
+
+.childCardContainer .childCard .actions {
+    background-color: #0070f3;
+    padding: 2rem 0.5rem 1rem;
+    color: white;
+    display: flex;
+    justify-content: space-around;
+    border-radius: 12px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+
+.childCardContainer .childCard .actions span {
+    border: 1px solid #0070f3;
+    background-color: white;
+    color: #0070f3;
+    padding: 0.3rem 0.9rem;
+    border-radius: 12px;
+    cursor: pointer;
+}
